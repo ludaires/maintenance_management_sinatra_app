3601f0c46af1f77b939fb3a9913d96fa9b9a3359
ludaires <ludaires@gmail.com>
2018-08-22 12:41:11 -0400

    add validates to email, username presence anda email format.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
     has_secure_password
-
     has_many :maintenances
+
+    validates :email, presence: true
+    validates :username, presence: true
+    # email format is correct
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
 
 end
