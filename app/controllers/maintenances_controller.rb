cb43f40ff025783604c96b96e2220b55184994dc
ludaires <ludaires@gmail.com>
2018-08-23 22:27:10 -0400

    Refactoring: Maintenances

--- app/controllers/maintenances_controller.rb
+++ app/controllers/maintenances_controller.rb
@@ -2,31 +2,20 @@ class MaintenancesController < ApplicationController
 
 
     get '/maintenances/new' do
-        if logged_in? 
-            @user = current_user
+        if logged_in?             
             erb :'/maintenances/create_maintenance'
         else
             redirect :'/users/login'
         end
     end
 
-    post '/maintenances/new' do
-        # raise params.inspect
-        # binding.pry
+    post '/maintenances' do        
         @maintenance = Maintenance.create(params)
-        @equipment = Equipment.find_by_id(params[:equipment_id])
-        @user = current_user
-        @user.maintenances << @maintenance
-        @equipment.maintenances << @maintenance
-
         redirect :"/maintenances/#{@maintenance.id}"
     end
 
     get '/maintenances/:id' do
-        @maintenance = Maintenance.find_by_id(params[:id])
-        @user = @maintenance.user
-        @equipment = @maintenance.equipment
-
+        @maintenance = Maintenance.find(params[:id])
         erb :'/maintenances/show_maintenance'
     end
 
