885abbfef7bf3dcb89f33039cc04142de489d627
ludaires <ludaires@gmail.com>
2018-08-27 14:28:34 -0400

    Restructure the creation maintenance as how to show it afterwords.

--- app/controllers/maintenances_controller.rb
+++ app/controllers/maintenances_controller.rb
@@ -10,12 +10,15 @@ class MaintenancesController < ApplicationController
     end
 
     post '/maintenances' do        
-        @maintenance = Maintenance.create(params)
+        @maintenance = Maintenance.create(params[:maintenance])
+        @inspection = Inspection.create(params[:inspection])
+        @inspection.update(maintenance_id: @maintenance.id)
         redirect :"/maintenances/#{@maintenance.id}"
     end
 
     get '/maintenances/:id' do
         @maintenance = Maintenance.find(params[:id])
+        @inspection = Inspection.find(@maintenance.id)
         @issues = Code.where(code_type: "Problemas")
         @causes = Code.where(code_type: "Causas")
         @actions = Code.where(code_type: "Ações")
@@ -23,11 +26,24 @@ class MaintenancesController < ApplicationController
     end
 
     post '/maintenances/:id/inspections' do
-        @inspection = Inspection.create(params[:inspection])
+        @inspection = Inspection.find(params[:inspection][:maintenance_id])
         @inspection_part = InspectionPart.create(inspection_id: @inspection.id,part_id: params[:inspection_parts][:part_id])
         codes = "#{params[:inspection_parts][:"code_id_1"]}, #{params[:inspection_parts][:"code_id_2"]}, #{params[:inspection_parts][:"code_id_3"]}"
         @inspection_part.update(codes: codes)
         redirect :"/inspection_parts/#{@inspection_part.id}"
     end
+
+    get '/maintenances/:id/edit' do
+        # raise params.inspect
+        @maintenance = Maintenance.find(params[:id])
+        erb :"/maintenances/edit_maintenance"
+    end
+
+    patch '/maintenances/:id' do
+        @maintenance = Maintenance.find(params[:id])
+        @maintenance.update(status: params[:status])
+        redirect :"/maintenances/#{@maintenance.id}"
+    end
+
 
 end
