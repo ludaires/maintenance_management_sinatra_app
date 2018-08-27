919e7bf570b5c198e2fd5c3b1413db81fab227eb
ludaires <ludaires@gmail.com>
2018-08-26 22:58:23 -0400

    Create variables to separete the code_type on the webpage, and create a post '/maintenances/:id/inspections' route.

--- app/controllers/maintenances_controller.rb
+++ app/controllers/maintenances_controller.rb
@@ -16,10 +16,18 @@ class MaintenancesController < ApplicationController
 
     get '/maintenances/:id' do
         @maintenance = Maintenance.find(params[:id])
+        @issues = Code.where(code_type: "Problemas")
+        @causes = Code.where(code_type: "Causas")
+        @actions = Code.where(code_type: "Ações")
         erb :'/maintenances/show_maintenance'
     end
 
-
-
+    post '/maintenances/:id/inspections' do
+        @inspection = Inspection.create(params[:inspection])
+        @inspection_part = InspectionPart.create(inspection_id: @inspection.id,part_id: params[:inspection_parts][:part_id])
+        codes = "#{params[:inspection_parts][:"code_id_1"]}, #{params[:inspection_parts][:"code_id_2"]}, #{params[:inspection_parts][:"code_id_3"]}"
+        @inspection_part.update(codes: codes)
+        redirect :"/inspection_parts/#{@inspection_part.id}"
+    end
 
 end
