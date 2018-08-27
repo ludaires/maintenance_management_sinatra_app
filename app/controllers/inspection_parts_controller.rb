16390f07cb76139b4f569de330bdb877e484b543
ludaires <ludaires@gmail.com>
2018-08-27 15:48:28 -0400

    Users can only create/view/edit inspections parts if the instance belongs to them.

--- app/controllers/inspection_parts_controller.rb
+++ app/controllers/inspection_parts_controller.rb
@@ -2,7 +2,12 @@ class InspectionPartsController < ApplicationController
 
     get '/inspection_parts/:id' do
         @inspection_part = InspectionPart.find(params[:id])
-        erb :'/inspection_parts/show_inspection_part'
+        @user = @inspection_part.inspection.maintenance.user
+        if logged_in? && @user.id == current_user.id
+            erb :'/inspection_parts/show_inspection_part'
+        else
+            flash[:message] = "Your user doesn't have the permission to edit this inspection part."
+            redirect :'/users/login'
+        end
     end
-
 end
