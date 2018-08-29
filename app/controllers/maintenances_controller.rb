61664ca961a79ce680d08639214c938fb93d7b09
ludaires <ludaires@gmail.com>
2018-08-29 17:32:33 -0400

    Add coments, delete route. In post /maintenances/:id/inspections implement changes only by the user who create the maintenance. Add a check to verify if the equipment's already has a maintenance not concluded.

--- app/controllers/maintenances_controller.rb
+++ app/controllers/maintenances_controller.rb
@@ -1,6 +1,6 @@
 class MaintenancesController < ApplicationController
 
-
+# create new maintenance
     get '/maintenances/new' do
         if logged_in?             
             erb :'/maintenances/create_maintenance'
@@ -10,34 +10,64 @@ class MaintenancesController < ApplicationController
     end
 
     post '/maintenances' do        
-        @maintenance = Maintenance.create(params[:maintenance])
-        @inspection = Inspection.create(params[:inspection])
-        @inspection.update(maintenance_id: @maintenance.id)
-        redirect :"/maintenances/#{@maintenance.id}"
+        @maintenance = Maintenance.new(params[:maintenance])
+        @equipment = Equipment.find_by(id: @maintenance.equipment.id)
+        @equipment.maintenances.each do |m|
+            @eq_in_maintenance = m.status
+            @eq_maintenance_id = m.id
+        end
+        # binding.pry
+        if @maintenance.status == "in_progress"  && !@eq_in_maintenance == "in_progress"
+            @maintenance.save
+            if @maintenance.save
+                @inspection = Inspection.create(params[:inspection])
+                @inspection.update(maintenance_id: @maintenance.id)
+                redirect :"/maintenances/#{@maintenance.id}"
+            else
+                flash[:message] = "Something went wrong: #{@maintenance.errors.full_messages.to_sentence}"
+                redirect :'/maintenances/new'
+            end
+        else
+            flash[:message] = "You need to close this maintenance to create a new one."
+            redirect :"/maintenances/#{@eq_maintenance_id}"
+        end
     end
 
+    get '/maintenances' do
+        erb :'/maintenances/index_maintenances'
+    end
+    
+# add maintenance details using inspection and inspection_parts models.
     get '/maintenances/:id' do
         @maintenance = Maintenance.find(params[:id])
-        if logged_in? && @maintenance.user.id == current_user.id
+        @inspection = Inspection.find(@maintenance.id)
+        # if logged_in? && @maintenance.user.id == current_user.id
             @inspection = Inspection.find(@maintenance.id)
             @issues = Code.where(code_type: "Problemas")
             @causes = Code.where(code_type: "Causas")
             @actions = Code.where(code_type: "Ações")
             erb :'/maintenances/show_maintenance'
+        # else
+        #     flash[:message] = "Your user doesn't have the permission to edit this maintenance."
+        #     redirect :'/users/login'
+        # end
+    end
+
+    post '/maintenances/:id/inspections' do
+        @inspection = Inspection.find(params[:inspection][:maintenance_id])
+        @maintenance = Maintenance.find(params[:id])
+        if logged_in? && @maintenance.user.id == current_user.id
+            @inspection_part = InspectionPart.create(inspection_id: @inspection.id,part_id: params[:inspection_parts][:part_id])
+            codes = "#{params[:inspection_parts][:"code_id_1"]}, #{params[:inspection_parts][:"code_id_2"]}, #{params[:inspection_parts][:"code_id_3"]}"
+            @inspection_part.update(codes: codes)
+            redirect :"/inspection_parts/#{@inspection_part.id}"
         else
             flash[:message] = "Your user doesn't have the permission to edit this maintenance."
             redirect :'/users/login'
         end
     end
 
-    post '/maintenances/:id/inspections' do
-        @inspection = Inspection.find(params[:inspection][:maintenance_id])
-        @inspection_part = InspectionPart.create(inspection_id: @inspection.id,part_id: params[:inspection_parts][:part_id])
-        codes = "#{params[:inspection_parts][:"code_id_1"]}, #{params[:inspection_parts][:"code_id_2"]}, #{params[:inspection_parts][:"code_id_3"]}"
-        @inspection_part.update(codes: codes)
-        redirect :"/inspection_parts/#{@inspection_part.id}"
-    end
-
+    # edit maintenance - close status
     get '/maintenances/:id/edit' do
         @maintenance = Maintenance.find(params[:id])
         if logged_in? && @maintenance.user.id == current_user.id
@@ -54,5 +84,17 @@ class MaintenancesController < ApplicationController
         redirect :"/maintenances/#{@maintenance.id}"
     end
 
+    delete '/maintenances/:id/delete' do
+        @maintenance = Maintenance.find(params[:id])
+       if logged_in? && @maintenance.user.id == current_user.id
+            @maintenance = Maintenance.find(params[:id])
+            @maintenance.destroy
+            flash[:message] = "Delete successful!"
+            redirect :"/users/#{@maintenance.user.id}"
+       else
+            flash[:message] = "Error. The user id does not match. Only the creator of the entry can delete this."
+            redirect :"/maintenances/#{@maintenance.id}"
+       end
+    end
 
 end
