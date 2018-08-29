c31207773e451224f023ed5ffaf91ade6e80ea59
ludaires <ludaires@gmail.com>
2018-08-29 18:03:42 -0400

    Correct bugs on erb and redirect.

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -8,7 +8,7 @@ class CodesController < ApplicationController
     post '/new' do
         if params[:code].present? && params[:code_type].present? && params[:title].present?
             @c = Code.create(params)
-            erb :'/codes/show'
+            redirect :"/codes/#{@c.id}"
         else
             flash[:message] = "You need to complete all fields "
             redirect :'codes/new'
@@ -25,10 +25,11 @@ class CodesController < ApplicationController
     patch '/codes/:id' do
         @c = Code.find_by(id: params[:id])
         if params[:code].present? && params[:code_type].present? && params[:title].present?
-            binding.pry
+            # binding.pry
             @c.update(code: params[:code], code_type: params[:code_type], title: params[:title])
-            erb :'/codes/show'
+            redirect :"/codes/#{@c.id}"
         else
+            flash[:message] = "You need to type all fields"
             redirect :"/codes/#{@c.id}/edit" 
         end
       
@@ -36,15 +37,16 @@ class CodesController < ApplicationController
 
 
     # delete action
-    get '/code/:id/delete' do
-        erb :'/codes/delete_code'
-    end
-    
+   
     delete '/codes/:id/delete' do
-        @c = Code.find(params[:id])
-        @c.delete
+        @c = Code.find_by(id: params[:id])
+        @c.destroy
+        redirect :'/codes/new'
     end
 
-
+    get '/codes/:id' do
+        @c = Code.find_by(id: params[:id])
+        erb :'/codes/show_code'
+    end
 
 end
