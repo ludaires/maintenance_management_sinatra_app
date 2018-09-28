9b48c37d809083ce567113429112293a77973187
Ludmilla Aires Negri <ludaires@gmail.com>
2018-09-27 23:30:43 -0400

    Add edit to Inspection Parts.

--- app/controllers/inspection_parts_controller.rb
+++ app/controllers/inspection_parts_controller.rb
@@ -10,4 +10,25 @@ class InspectionPartsController < ApplicationController
             redirect :'/users/login'
         end
     end
+
+# edit inspection parts
+
+    get '/inspection_parts/:id/edit' do
+        @inspection_part = InspectionPart.find(params[:id])
+        @issues = Code.where(code_type: "Problemas")
+        @causes = Code.where(code_type: "Causas")
+        @actions = Code.where(code_type: "Ações")
+        erb :'/inspection_parts/edit_inspection_part'
+    end
+
+    patch '/inspection_parts/:id' do
+        @inspection_part = InspectionPart.find(params[:id])
+        @inspection = Inspection.find(@inspection_part.inspection.id)
+        @inspection_part.update(part_id: params[:inspection_parts][:part_id])
+        codes = "#{params[:inspection_parts][:"code_id_1"]}, #{params[:inspection_parts][:"code_id_2"]}, #{params[:inspection_parts][:"code_id_3"]}"
+        @inspection_part.update(codes: codes)
+        redirect :"/inspection_parts/#{@inspection_part.id}"
+    end
+
+
 end
