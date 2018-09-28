9999870e11a6b41b4d162cac860197b291528b83
Ludmilla Aires Negri <ludaires@gmail.com>
2018-09-27 21:40:36 -0400

    add flux control to open a maintenance.

--- app/controllers/maintenances_controller.rb
+++ app/controllers/maintenances_controller.rb
@@ -12,24 +12,27 @@ class MaintenancesController < ApplicationController
     post '/maintenances' do        
         @maintenance = Maintenance.new(params[:maintenance])
         @equipment = Equipment.find_by(id: @maintenance.equipment.id)
+        # Checking equipment maintenance status to make sure we are not creating a new maintenance without closing the old one.
         @equipment.maintenances.each do |m|
             @eq_in_maintenance = m.status
             @eq_maintenance_id = m.id
         end
-        # binding.pry
-        if @maintenance.status == "in_progress"  && !@eq_in_maintenance == "in_progress"
+        # checking if we have a open maintenance
+        if  @eq_in_maintenance == "in_progress"
+            flash[:message] = "You need to close this maintenance to create a new one."
+            redirect :"/maintenances/#{@eq_maintenance_id}"
+        # checking in case we don't have a open maintenance.
+        elsif @eq_in_maintenance != "in_progress"
             @maintenance.save
             if @maintenance.save
                 @inspection = Inspection.create(params[:inspection])
                 @inspection.update(maintenance_id: @maintenance.id)
                 redirect :"/maintenances/#{@maintenance.id}"
+            # ckecking in case a user don't fill it up all the requirements to open a maintenance. 
             else
                 flash[:message] = "Something went wrong: #{@maintenance.errors.full_messages.to_sentence}"
                 redirect :'/maintenances/new'
             end
-        else
-            flash[:message] = "You need to close this maintenance to create a new one."
-            redirect :"/maintenances/#{@eq_maintenance_id}"
         end
     end
 
@@ -41,30 +44,29 @@ class MaintenancesController < ApplicationController
     get '/maintenances/:id' do
         @maintenance = Maintenance.find(params[:id])
         @inspection = Inspection.find(@maintenance.id)
-        # if logged_in? && @maintenance.user.id == current_user.id
-            @inspection = Inspection.find(@maintenance.id)
+        if logged_in? && @maintenance.user.id == current_user.id
             @issues = Code.where(code_type: "Problemas")
             @causes = Code.where(code_type: "Causas")
             @actions = Code.where(code_type: "Ações")
             erb :'/maintenances/show_maintenance'
-        # else
-        #     flash[:message] = "Your user doesn't have the permission to edit this maintenance."
-        #     redirect :'/users/login'
-        # end
+        else
+            flash[:message] = "Your user doesn't have the permission to edit this maintenance."
+            redirect :'/users/login'
+        end
     end
 
     post '/maintenances/:id/inspections' do
         @inspection = Inspection.find(params[:inspection][:maintenance_id])
         @maintenance = Maintenance.find(params[:id])
-        if logged_in? && @maintenance.user.id == current_user.id
-            @inspection_part = InspectionPart.create(inspection_id: @inspection.id,part_id: params[:inspection_parts][:part_id])
+        # if logged_in? && @maintenance.user.id == current_user.id
+            @inspection_part = InspectionPart.create(inspection_id: @inspection.id, part_id: params[:inspection_parts][:part_id])
             codes = "#{params[:inspection_parts][:"code_id_1"]}, #{params[:inspection_parts][:"code_id_2"]}, #{params[:inspection_parts][:"code_id_3"]}"
             @inspection_part.update(codes: codes)
             redirect :"/inspection_parts/#{@inspection_part.id}"
-        else
-            flash[:message] = "Your user doesn't have the permission to edit this maintenance."
-            redirect :'/users/login'
-        end
+        # else
+            # flash[:message] = "Your user doesn't have the permission to edit this maintenance."
+            # redirect :'/users/login'
+        # end
     end
 
     # edit maintenance - close status
